f7d6fb3993ce6e1d323889e43bb198eb58c959e8
fwangjuwon <fwangjuwon@naver.com>
2022-05-19 17:42:12 +0900

    주소api로 도로명검색기능 추가

--- src/main/java/site/metacoding/logintest/web/UserController.java
+++ src/main/java/site/metacoding/logintest/web/UserController.java
@@ -5,9 +5,8 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
-import site.metacoding.logintest.config.auth.LoginUser;
 import site.metacoding.logintest.handler.ex.CustomException;
 import site.metacoding.logintest.service.UserService;
 import site.metacoding.logintest.web.dto.JoinReqDto;
@@ -29,17 +27,17 @@ public class UserController {
     
     @GetMapping("/join-form")
     public String joinForm() {
-        return "joinForm";
+        return "/joinForm";
     }
 
     @GetMapping("/login-form")
     public String loginForm() {
-        return "loginForm";
+        return "/loginForm";
     }
 
     @GetMapping("/user/{id}")
     public String updateForm(@PathVariable Integer id) {
-        return "updateForm";
+        return "/updateForm";
     }
 
      @PostMapping("/join")
@@ -56,6 +54,18 @@ public class UserController {
          userService.회원가입(joinReqDto.toEntity());
 
          return "redirect:/login-form";
+     }
+
+     @GetMapping("/juso-popup")
+     public String jusoPopup() {
+         return "/jusoPopup";
+     }
+
+     @PostMapping("/juso-popup")
+     public String juso(String inputYn, String roadFullAddr, Model model) {
+         model.addAttribute("inputYn", inputYn);
+         model.addAttribute("roadFullAddr", roadFullAddr);
+               return "/jusoPopup";
      }
     
 }
