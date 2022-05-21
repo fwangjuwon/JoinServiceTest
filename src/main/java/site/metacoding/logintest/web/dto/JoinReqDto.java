3729c453bd32e6d8d311e7aee77dd6cc15c8c88e
fwangjuwon <fwangjuwon@naver.com>
2022-05-21 22:51:33 +0900

    회원정보 수정 버그 수정 완료

--- src/main/java/site/metacoding/logintest/web/dto/JoinReqDto.java
+++ src/main/java/site/metacoding/logintest/web/dto/JoinReqDto.java
@@ -2,7 +2,6 @@ package site.metacoding.logintest.web.dto;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
